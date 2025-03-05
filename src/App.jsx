8fde148de70ecce551877a1777a7f4c9c69fafdf
prabin bagale <prabinbagale08@gmail.com>
2025-03-05 23:05:00 +0545

    setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
+import Dashboard from './components/Dashboard';
+
 
 function App() {
   return (
@@ -10,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
