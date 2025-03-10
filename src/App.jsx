0a4aaba31b431a885ae4a09f83aeea41c8e12830
prabin bagale <prabinbagale08@gmail.com>
2025-03-10 12:39:53 +0545

    add queryClientprovider

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; // Import necessary modules
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 import Dashboard from './components/Dashboard';
+import { AuthProvider } from './context/AuthContext';
 
+const queryClient = new QueryClient(); // Create a new QueryClient instance
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}> {/* Wrap your app with QueryClientProvider */}
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AuthProvider>
+      </QueryClientProvider>
     </Router>
   );
 }
