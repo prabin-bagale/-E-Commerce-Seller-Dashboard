c5f94f026b1baac9d034224f67c12435dfa870d0
prabin bagale <prabinbagale08@gmail.com>
2025-03-11 11:15:52 +0545

    create csvexport file

---
+++ src/utils/csvExport.jsx
@@ -0,0 +1,16 @@
+export const exportToCSV = (data, filename) => {
+    const csvContent = [
+      Object.keys(data[0]).join(','),
+      ...data.map(item => Object.values(item).join(','))
+    ].join('\n');
+  
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', filename);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+  
