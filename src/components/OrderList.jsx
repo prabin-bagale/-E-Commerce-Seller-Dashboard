11ac793d4ce3324950a4d51d7f16d85c05372d93
prabin bagale <prabinbagale08@gmail.com>
2025-03-11 11:14:39 +0545

    create Orderlist component

---
+++ src/components/OrderList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OrderList = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Orders</h2>
+      <p>Order management coming soon...</p>
+    </div>
+  );
+};
+
+export default OrderList;
